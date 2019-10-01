bdad9b0b16b928332ec3a47a292bfe72f13e3909
hoangphilong <hoangphihong1422000@gmail.com>
2019-10-01 17:48:19 +0700

    CeilFloor

--- tet/UnitTestProject1/UnitTest1.cs
+++ tet/UnitTestProject1/UnitTest1.cs
@@ -1,7 +1,8 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using tet;
 
-namespace UnitTestProject1
+namespace tet
 {
     [TestClass]
     public class UnitTest1
@@ -9,6 +10,12 @@ namespace UnitTestProject1
         [TestMethod]
         public void TestMethod1()
         {
+            int result1 = Program.Floor((float)2.3);
+            Assert.AreEqual(2, result1);
+            int result2 = Program.Floor((float)-3.4);
+            Assert.AreEqual(-4, result2);
+            int result3 = Program.Floor(3);
+            Assert.AreEqual(3, result3);
         }
     }
 }
--- tet/tet/Program.cs
+++ tet/tet/Program.cs
@@ -6,17 +6,95 @@ using System.Threading.Tasks;
 
 namespace tet
 {
-    class Program
+    public class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine(Square(2));
+            Console.WriteLine(Cube(2));
+            Console.WriteLine(Even(2));
+            Console.WriteLine(Odd(2));
+            Console.WriteLine(IsPrime(2));
         }
         static int Square(int n)
         {
             int Square = 0;
             Square = n * n;
             return Square;
+        }
+        static int Cube(int n)
+        {
+            int Cube = 0;
+            Cube = Square(n) * n;
+            return Cube;
+        }
+        static bool IsPrime(int n)
+        {
+            int count =0;
+            for (int i = 1; i <= n; i++)
+            {
+                if (n % i== 0)
+                {
+                    count++;
+                }
+            } if (count == 2)
+            {
+                return true;
+            }
+            
+            return false;
+        }
+        static bool Even(int n)
+        {
+            if (n % 2 == 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        static bool Odd(int n)
+        {
+            if (n % 2 != 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        public static int Ceil(float x)
+        {
+            int Ceil = (int)x+1;
+            if (x<0) 
+            {
+                Ceil = (int)x;   
+            }
+            if (x > 0)
+            {
+                if (x > Ceil)
+                {
+                    Ceil = Ceil + 1;
+                }
+            }
+            return Ceil;
+        }
+        //input: 
+        public static int Floor(float x)
+        {
+            int Floor = (int)x-1;
+            if (x < 0)
+            {
+                Floor = (int)x - 1;
+            }
+            if (x > Floor)
+            {
+                Floor = Floor + 1;
+            }
+            return Floor;
         }
     }
 }
