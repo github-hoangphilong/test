bf2076d7411ee90ca16698c0ad5f9dc32fd3d88f
hoangphilong <hoangphihong1422000@gmail.com>
2019-10-01 17:48:01 +0700

    CeilFloor

---
+++ tet/tet/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Square(2));
+        }
+        static int Square(int n)
+        {
+            int Square = 0;
+            Square = n * n;
+            return Square;
+        }
+    }
+}
